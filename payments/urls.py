640e83b6ff0e519594df2873db506dddcea31347
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-27 12:58:52 -0300

    Sending payments with pix.

--- payments/urls.py
+++ payments/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('processpayment/', views.processpayment, name='processpayment'),
+    path('pending/', views.resultpayment, name='resultpayment'),
 
 ]
--- payments/views.py
+++ payments/views.py
@@ -3,7 +3,8 @@ import os
 
 import mercadopago
 from django.conf import settings
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 
 sdk = mercadopago.SDK(settings.ACCESS_TOKEN)
@@ -12,36 +13,44 @@ sdk = mercadopago.SDK(settings.ACCESS_TOKEN)
 
 
 def home(request):
-    template_name = 'payments/pages/home.html'
+    if request.method == 'GET':
+        template_name = 'payments/pages/home.html'
+        context = {
+            'public_key': os.environ.get('PUBLIC_KEY'),
+        }
+        return render(request, template_name, context)
+
+
+def processpayment(request):
+    if request.method == 'POST':
+        data_json = request.body
+        data = json.loads(data_json)
+        payment_data = {
+            "transaction_amount": float(data["transaction_amount"]),
+            "installments": 1,
+            "payment_method_id": data["payment_method_id"],
+            "payer": {
+                "email": data["payer"]["email"],
+                "identification": {
+                    "type": "CPF",
+                    "number": "191191191-00",
+                }
+            }
+        }
+
+        payment_response = sdk.payment().create(payment_data)
+        payment = payment_response["response"]
+
+        print("status =>", payment["status"])
+        print("status_detail =>", payment["status_detail"])
+        print("id =>", payment["id"])
+        response = redirect('/pending/')
+        return response
+
+
+def resultpayment(request):
+    template_name = 'payments/pages/resultpayments.html'
     context = {
         'public_key': os.environ.get('PUBLIC_KEY'),
     }
     return render(request, template_name, context)
-
-
-@csrf_exempt
-def processpayment(request):
-    data_json = request.body
-    data = json.loads(data_json)
-    payment_data = {
-        "transaction_amount": float(data["transaction_amount"]),
-        "token": data["token"],
-        "installments": int(data["installments"]),
-        "payment_method_id": data["payment_method_id"],
-        "issuer_id": data["issuer_id"],
-        "payer": {
-            "email": data["payer"]["email"],
-            "identification": {
-                "type": data["payer"]["identification"]["type"],
-                "number": data["payer"]["identification"]["number"]
-            }
-        }
-    }
-
-    payment_response = sdk.payment().create(payment_data)
-    payment = payment_response["response"]
-
-    print("status =>", payment["status"])
-    print("status_detail =>", payment["status_detail"])
-    print("id =>", payment["id"])
-    return render(request, 'payments/pages/home.html')
